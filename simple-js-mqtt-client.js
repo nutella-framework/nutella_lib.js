d6c5f51a3c9fe049e2129ad8ba085a8a4ed94d99
Alessandro Gnoli <tebemis@gmail.com>
2015-02-17 12:06:09 -0600

    Added mqtt client

---
+++ simple-js-mqtt-client.js
@@ -0,0 +1,427 @@
+/**********************
+ * Simple MQTT client *
+ **********************/
+
+(function () {
+	"use strict";
+
+	// Establish the root object, `window` in the browser, or `exports` on the server.
+	var root = this;
+
+	// Save the previous value of the `MQTT` variable to use with noConflict().
+	var previous_mqtt = root.MQTT;
+
+    // Internal reference to this library (used below)
+    var mqtt = {};
+
+	// Load dependencies (mqtt libraries)
+	// adamvr/MQTT.js for node (https://github.com/adamvr/MQTT.js)
+	// Paho.js, via mqtt-ws, for the browser (https://github.com/M2MConnections/mqtt-ws)
+
+	// Detect if we are in the browser or in node and
+    // load the appropriate dependencies
+    var isNode;
+    var mqtt_lib;
+	if (typeof window === 'undefined') {
+		isNode = true;		// Node
+		mqtt_lib = require('mqtt');
+        if( typeof mqtt_lib === 'undefined' )
+            throw new Error('This MQTT client requires the mqtt library (https://www.npmjs.com/package/mqtt)');
+	} else {
+		isNode = false;		// Browser
+		mqtt_lib = root.Paho.MQTT;
+		if( typeof mqtt_lib === 'undefined' )
+            throw new Error('This MQTT client requires the mqtt-ws library (https://github.com/M2MConnections/mqtt-ws) a wrapper of Paho.js');
+
+	}
+
+
+
+	/**
+	 * Runs simple-js-mqtt-client in noConflict mode by
+	 * returning the MQTT variable to its previous owner.
+	 * @return  a reference to the MQTT object defined by this library.
+	 */
+	mqtt.noConflict = function() {
+		root.MQTT = previous_mqtt;
+	  return mqtt;
+	};
+
+
+
+	/**
+	 * Creates a new instance of SimpleMQTTClient
+	 * and connects it to an MQTT.
+     * This is a factory method.
+	 *
+	 * @param {string} host - the hostname of the broker.
+	 * @param {string} [clientId]  - the unique name of this client. If no ID is provided a random one is generated
+	 */
+	mqtt.connect = function (host, clientId) {
+		return new SimpleMQTTClient(host, clientId);
+	};
+
+
+
+	/**
+	 * Defines a Simple MQTT client.
+     *
+	 * @param {string} host - the hostname of the broker.
+	 * @param {string} [clientId]  - the unique name of this client. If no ID is provided a random one is generated
+	 */
+    var SimpleMQTTClient = function (host, clientId) {
+
+        // Initializes the object that stores subscriptions
+        this.subscriptions = {};
+        // Initializes the object that holds the internal client
+        this.client = {};
+        // Functions backlog
+        this.backlog = [];
+
+        // Handles the optional clientId parameter
+        if (arguments.length === 1 || clientId === undefined) {
+            clientId = generateRandomClientId();
+        }
+
+        // Connect
+        if (isNode)
+            this.client = connectNode(this.subscriptions, this.backlog, host, clientId);
+        else
+            this.client = connectBrowser(this.subscriptions, this.backlog, host, clientId);
+    };
+
+
+	//
+	// Helper function that connects the MQTT client in node
+    // TODO refactor
+	//
+	function connectNode (subscriptions, backlog, host, clientId) {
+		// Create client
+		var url = "tcp://" + host + ":1883";
+		var client = mqtt.connect(url, {clientId : clientId});
+		// Register incoming message callback
+		client.on('message', function(channel, message) {
+			// Executes the appropriate channel callback
+			var cb = findCallback(subscriptions, channel);
+			if (cb!==undefined) {
+					Object.keys(subscriptions).indexOf(channel)!==-1 ? cb(message) : cb(message, channel);
+			}
+		});
+		// Register successful connection callback
+		client.on('connect', function() {
+			// Execute the backlog of operations performed while the client wasn't connected
+			backlog.forEach(function(e) {
+				e.op.apply(this, e.params);
+			});
+		});
+	}
+
+
+	//
+	// Helper function that connects the MQTT client in the browser
+	//
+	function connectBrowser (subscriptions, backlog, host, clientId) {
+		// Create client
+		var client = new mqtt_lib.Client(host, Number(1884), clientId);
+		// Register callback for connection lost
+		client.onConnectionLost = function() {
+			// TODO try to reconnect
+		};
+		// Register callback for received message
+		client.onMessageArrived = function (message) {
+			// Executes the appropriate channel callback
+			var cb = findCallback(subscriptions, message.destinationName);
+			if (cb!==undefined) {
+					Object.keys(subscriptions).indexOf(message.destinationName)!==-1 ? cb(message.payloadString) : cb(message.payloadString, message.destinationName);
+			}
+		};
+		// Connect
+		client.connect({onSuccess: function() {
+			// Execute the backlog of operations performed while the client wasn't connected
+			backlog.forEach(function(e) {
+				e.op.apply(this, e.params);
+			});
+		}});
+		return client;
+	}
+
+
+
+    /**
+     * Disconnects from the MQTT client.
+     */
+    SimpleMQTTClient.prototype.disconnect = function () {
+        if (isNode)
+            this.client.end();
+        else
+            this.client.disconnect();
+        this.subscriptions = {};
+    };
+
+
+
+	/**
+	 * Subscribes to a channel and registers a callback.
+     *
+	 * @param {string} channel  - the channel we are subscribing to.
+	 * @param {callback} callback - A function that is executed every time a message is received on that channel.
+	 * @param {callback} [done_callback] - A function that is executed once the subscribe operation has completed successfully.
+	 */
+	SimpleMQTTClient.prototype.subscribe = function (channel, callback, done_callback) {
+		// Prevent from subscribing twice to a channel
+		if (Object.keys(this.subscriptions).indexOf(channel)!==-1) {
+			console.error("You can only subscribe to a channel once");
+			return;
+		}
+		// Subscribe
+		if( isNode )
+			subscribeNode(this.client, this.subscriptions, this.backlog, channel, callback, done_callback);
+		else
+			subscribeBrowser(this.client, this.subscriptions, this.backlog, channel, callback, done_callback);
+	};
+
+
+	//
+	// Helper function that subscribes to a channel in node
+    // TODO refactor
+	//
+	function subscribeNode (channel, callback, done_callback) {
+		var onSuccess = function(err, granted) {
+			subscriptions[channel] = callback;
+			// If there is a done_callback defined, execute it
+			if (done_callback!==undefined) {
+				done_callback();
+			}
+		};
+		n_client.subscribe(channel, {qos : 0}, onSuccess);
+	}
+
+
+	//
+	// Helper function that subscribes to a channel in the browser
+	//
+	function subscribeBrowser (client, subscriptions, backlog, channel, callback, done_callback) {
+		if ( addToBacklog(client, backlog, subscribeBrowser, [client, subscriptions, backlog, channel, callback, done_callback]) ) return;
+		var options = {};
+		options.qos = 0;
+		options.onSuccess = function() {
+			subscriptions[channel] = callback;
+			// If there is a done_callback defined, execute it
+			if (done_callback!==undefined) {
+				done_callback();
+			}
+		};
+		client.subscribe(channel, options);
+	}
+
+
+
+	/**
+	 * Unsubscribe from a channel.
+     *
+	 * @param {string} channel  - the channel we are unsubscribing from.
+	 * @param {callback} [done_callback] - A function that is executed once the unsubscribe operation has completed successfully.
+	 */
+	SimpleMQTTClient.prototype.unsubscribe = function (channel, done_callback) {
+		if( isNode )
+			unsubscribeNode(channel, done_callback);
+		else
+            unsubscribeBrowser(this.client, this.subscriptions, this.backlog, channel, done_callback);
+	};
+
+
+    //
+	// Helper function that unsubscribes from a channel in node
+    // TODO refactor
+    //
+	var unsubscribeNode = function(channel, done_callback) {
+		var onSuccess = function() {
+			delete subscriptions[channel];
+			// If there is a done_callback defined, execute it
+			if (done_callback!==undefined) {
+				done_callback();
+			}
+		};
+		n_client.unsubscribe(channel, onSuccess);
+	};
+
+
+    //
+	// Helper function that unsubscribes from a channel in the browser
+    //
+	var unsubscribeBrowser = function(client, subscriptions, backlog, channel, done_callback) {
+		if ( addToBacklog(client, backlog, unsubscribeBrowser, [client, subscriptions, backlog, channel, done_callback]) ) return;
+		var options = {};
+		options.onSuccess = function() {
+			delete subscriptions[channel];
+			// If there is a done_callback defined, execute it
+			if (done_callback!==undefined) {
+				done_callback();
+			}
+		};
+		client.unsubscribe(channel, options);
+	};
+
+
+    /**
+     * Lists all the channels we are currently subscribed to.
+     *
+     * @returns {Array} a lists of all the channels we are currently subscribed to.
+     */
+    SimpleMQTTClient.prototype.getSubscriptions = function () {
+        return Object.keys(this.subscriptions);
+    };
+
+
+    /**
+     * Publishes a message to a channel.
+     *
+     * @param {string} channel  - the channel we are publishing to.
+     * @param {string} message - the message we are publishing.
+     */
+    SimpleMQTTClient.prototype.publish = function (channel, message) {
+        if (isNode)
+            publishNode(channel, message)
+        else
+            publishBrowser(this.client, this.backlog, channel, message)
+    };
+
+
+	//
+	// Helper function that publishes to a channel in node
+    // TODO refactor
+	//
+	var publishNode = function (channel, message) {
+		n_client.publish(channel, message)
+	};
+
+
+	//
+	// Helper function that publishes to a channel in the browser
+	//
+	var publishBrowser = function (client, backlog, channel, message) {
+		if ( addToBacklog(client, backlog, publishBrowser, [client, backlog, channel, message]) ) return;
+		message = new mqtt_lib.Message(message);
+		message.destinationName = channel;
+		client.send(message);
+	};
+
+
+    //
+    // Helper functions
+    //
+
+
+    //
+    // Helper function that generates a random client ID
+    //
+    function generateRandomClientId () {
+        var length = 22;
+        var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        var result = '';
+        for (var i = length; i > 0; --i) {
+            result += chars[Math.round(Math.random() * (chars.length - 1))];
+        }
+        return result;
+    };
+
+
+    //
+    // Helper function that selects the right callback when a message is received
+    //
+    function findCallback (subscriptions, channel) {
+        // First try to see if a callback for the exact channel exists
+        if(Object.keys(subscriptions).indexOf(channel)!==-1)
+            return subscriptions[channel];
+        // If it doesn't then let's try to see if the channel matches a wildcard callback
+        var pattern = matchesWildcard(subscriptions, channel);
+        if (pattern!== undefined) {
+            return subscriptions[pattern];
+        }
+        // If there's no exact match or wildcard we have to return undefined
+        return undefined;
+    };
+
+
+    //
+    // Helper function that tries to match a channel with each subscription
+    // it returns undefined if no match is found
+    //
+    function matchesWildcard (subscriptions, channel) {
+        var i;
+        var subs = Object.keys(subscriptions);
+        for (i=0; i < subs.length; i++) {
+            if (matchesFilter(subs[i], channel)) {
+                return subs[i];
+            }
+        }
+        return undefined;
+    };
+
+
+    //
+    // Helper function that checks a certain channel and see if it matches a wildcard pattern
+    // Returns true if the channel matches a pattern (including the exact pattern)
+    //
+    function matchesFilter (pattern, channel) {
+        // If multi-level wildcard is the only character in pattern, then any string will match
+        if (pattern==="#") {
+            return true;
+        }
+        // Handle all other multi-level wildcards
+        // FROM SPEC: The number sign (‘#’ U+0023) is a wildcard character that matches any number of levels within a topic. The multi-level wildcard represents the parent and any number of child levels. The multi-level wildcard character MUST be specified either on its own or following a topic level separator. In either case it MUST be the last character specified in the Topic Filter
+        var p_wo_wildcard = pattern.substring(0, pattern.length-2);
+        var str_wo_details = channel.substring(0, pattern.length-2);
+        if (pattern.slice(-1)=='#' && p_wo_wildcard==str_wo_details) {
+            return true;
+        }
+        // TODO Handle single-level wildcards (+)
+        // FROM SPEC: The single-level wildcard can be used at any level in the Topic Filter, including first and last levels. Where it is used it MUST occupy an entire level of the filter [MQTT-4.7.1-3]. It can be used at more than one level in the Topic Filter and can be used in conjunction with the multilevel wildcard.
+        // http://docs.oasis-open.org/mqtt/mqtt/v3.1.1/os/mqtt-v3.1.1-os.html#_Toc398718107
+        return false;
+    };
+
+
+    //
+    // Helper method that queues operations into the backlog.
+    // This method is used to make `connect` "synchronous" by
+    // queueing up operations on the client until it is connected.
+    //
+    // @param {string} method  - the method that needs to be added to the backlog
+    // @param {Array} parameters - parameters to the method being added to the backlog
+    // @returns {boolean} true if the method was successfully added, false otherwise
+    //
+    function addToBacklog (client, backlog, method, parameters) {
+        if (!client.isConnected() ) {
+            backlog.push({
+                op : method,
+                params : parameters
+            });
+            return true;
+        }
+        return false;
+    };
+
+
+
+
+    //
+	// End of methods definition for SimpleMQTTClient
+    //
+
+
+ 	// Exports mqtt object
+	// For node.js, also with backwards-compatibility for the old `require()` API.
+	// If we're in the browser, add `MQTT` as a global object.
+ 	if (typeof exports !== 'undefined') {
+ 		if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = mqtt;
+ 		}
+ 		exports.MQTT = mqtt;
+ 	} else {
+ 		root.MQTT = mqtt;
+ 	}
+
+
+
+}.call(this));
